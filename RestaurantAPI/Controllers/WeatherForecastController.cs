479619e09357c5b23d85ef2eb7ed590ae345035c
Mateusz Gunera <mateusy.gunera@wp.pl>
2022-04-03 20:59:25 +0200

    Add Restaurant Project

---
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IWeatherForecastService _service; 
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var result = _service.Get();
+        //    return result;
+        //}
+
+        //[HttpGet("currentDay/{max}")]
+        ////[Route("currentDay")]
+        //public IEnumerable<WeatherForecast> Get2([FromQuery]int take, [FromRoute]int max)
+        //{
+        //    var result = _service.Get();
+        //    return result;
+        //}
+
+        //[HttpPost]
+        //public ActionResult<string> Hellow([FromBody]string name)
+        //{
+        //    //HttpContext.Response.StatusCode = 401;
+            
+        //    //return StatusCode(401, $"Hellow {name}");
+
+        //    return NotFound($"Hellow {name}");
+        //}
+
+        [HttpPost("generate")]
+        public ActionResult<IEnumerable<WeatherForecast>> Generate([FromQuery] int count, [FromBody] TemperatureRange tempRange)
+        {
+            if (count < 0 || tempRange.Max < tempRange.Min)
+                return BadRequest();
+
+            var result = _service.Get(count, tempRange.Min, tempRange.Max);
+            return Ok(result);
+        }
+    }
+}
---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,104 @@
+﻿using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC is American fast food restaurant.",
+                    HasDelivery = true,
+                    ContactEmail = "contact@kfc.com",
+                    ContactNumber = "697899118",
+
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "B-smart",
+                            Price = 6.95M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 11.99M,
+                        },
+                    },
+
+                    Address = new Address()
+                    {
+                        City = "Szczecin",
+                        Street = "Potulicka",
+                        PostalCode = "234-56"
+                    },
+                },
+
+                new Restaurant()
+                {
+                    Name = "TienDat",
+                    Category = "Chinese cuisine",
+                    Description = "TienDat is American Chinese cuisine restaurant.",
+                    HasDelivery = true,
+                    ContactEmail = "tien-dat@td.com",
+                    ContactNumber = "798356758",
+
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Pad Thai",
+                            Price = 25.50M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Curry",
+                            Price = 23.99M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Crispy",
+                            Price = 24.20M,
+                        },
+                    },
+
+                    Address = new Address()
+                    {
+                        City = "Łobez",
+                        Street = "Niepodległości",
+                        PostalCode = "73-150"
+                    },
+                }
+            };
+
+            return restaurants;
+        }
+    }
+}
